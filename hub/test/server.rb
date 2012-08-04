65f0857e5214744f0ce1c61f5b5a3aed6707916e
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2012-08-04 16:58:59 +0200

    refine post exchange

--- hub/test/server.rb
+++ hub/test/server.rb
@@ -1,22 +1,33 @@
 require "amqp"
+require "json"
 
+puts "Starting."
 EventMachine.run do
   connection = AMQP.connect
   channel    = AMQP::Channel.new(connection)
 
   requests_queue = channel.queue("myownci.git.commit", :durable => true)
   requests_queue.subscribe(:ack => true) do |metadata, payload|
-    puts "[requests] Got a request #{metadata}. Sending a reply..."
-    puts "#{payload}"
-    channel.default_exchange.publish(Time.now.to_s,
-                                     :routing_key    => metadata.reply_to,
-                                     :correlation_id => metadata.message_id,
-                                     :immediate      => true,
-                                     :mandatory      => true)
+    puts "[commit] Got a request #{metadata.content_type}. Sending a reply..."
+    puts "metadata.routing_key : #{metadata.routing_key}"
+    puts "metadata.content_type: #{metadata.content_type}"
+    puts "metadata.headers     : #{metadata.headers.inspect}"
+    puts "metadata.timestamp   : #{metadata.timestamp.inspect}"
+    puts "metadata.delivery_tag: #{metadata.delivery_tag}"
+    puts "metadata.redelivered : #{metadata.redelivered?}"
 
+    puts "metadata.app_id      : #{metadata.app_id}"
+    puts
+    puts "Received a message:"
+    data = JSON.parser.new(payload).parse()
+    data.each{|k, v|
+      puts "#{k} => #{v}"
+    }
+    puts "."
     metadata.ack
   end
 
 
   Signal.trap("INT") { connection.close { EventMachine.stop } }
 end
+puts "End."
