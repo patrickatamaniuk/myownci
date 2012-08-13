edec089c76e9d9ba1441e30b41a783f9c7acbdef
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2012-08-13 11:47:52 +0200

    some matrix tests

--- hub/hub_frontend/spec/lib/matrix_spec.rb
+++ hub/hub_frontend/spec/lib/matrix_spec.rb
@@ -1,12 +1,42 @@
+require 'yaml'
 require 'hub/matrix'
 
 describe Hub::Matrix do
-  it "should be able to multiply a travis matrix" do
-    config = {
-        'language' => 'ruby'
-    }
+@data = [
+    {:count => 1, :yml => %Q|
+    language: ruby
+    |},
+
+    {:count => 1, :yml => %Q|
+    language: python
+    |},
+
+    {:count => 3, :yml => %Q|
+    language: ruby
+    rvm:
+    - "1.9.3"
+    - "1.8.7"
+    env:
+    - "e1"
+    - "e2"
+    matrix:
+      exclude:
+      - env: e1
+        rvm: "1.8.7"
+    |}
+]
+
+  index = 1
+  @data.each{|data|
+  config = YAML.load(data[:yml])
+  count = data[:count]
+  it "should be able to multiply a simple matrix" do
+    result = []
     Hub::Matrix.multiply(config){|job_arguments|
-      puts "#{job_arguments}"
+      result << job_arguments
     }
+    result.length.should == count
+    index = index + 1
   end
+  }
 end
